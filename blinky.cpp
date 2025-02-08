a111f1857f717f91616fa9cd5a7a455cf891f83a
andyh <andy.980f@gmail.com>
2025-02-07 22:19:36 -0600

    all working, <5k code and ~112 static data

--- blinky.cpp
+++ blinky.cpp
@@ -1,44 +1,41 @@
 /* emulate Arduino blinky application, to compare code size.
  */
 
-//#include "gpio.h"  //for Init, until that is automated.
-//#include "nvic.h"
-#include "softpwm.h"
-
-//shared ~millisecond services:  (probably should make the next pair of lines their own cpp file, to add to your build.
+//shared millisecond services:  (probably should make the next pair of lines their own cpp file, to add to your build.
 #include "msservice.h"
-RegisterPolledTimerWithSysTick
+RegisterPolledTimerWithSysTick;
+//above is like Arduino.h, will add more to it as we emulate the "always present" parts of Arduino, such as a default Serial.
 
-PolledSoftPWM ledToggle(750,250); 
-RegisterTimer(ledToggle);   //a polled timer, a macro for 'declare and register' could be made but it would have to be variadic to allow constructor args.
+//Sketch Start
+#include "softpwm.h"
+MakeTimer(PolledSoftPWM, ledToggler, 750,250); 
 
-#include "core_cmInstr.h" //wfe OR wfi
+
 #include "bluepill.h"  
-//#include "wtf.h"
-
 Bluepill board;
 
 void setup() {
   board.led=0;
-  ledToggle.trigger(1);//this calls the 'onDone' as if the given phase just completed. In this instance the low phase count will begin.
+  ledToggler.trigger(1);
 }
 
 void loop() {
-  board.led=!ledToggle;
+  board.led=!ledToggler;
 }
+//Sketch End
 
 //// below here is the equivalent of what Arduino build adds to your sketch:
+#include "core_cmInstr.h" //wfe/wfi
 int main(void) {
-
-  startPeriodicTimer(1000); //shoot for millisecond resolution
-  setup();//arduino heritage ;)
-
+  startPeriodicTimer(1000); //mimic Arduino millisecond standard timer services
+  setup();//more arduino heritage ;)
 
 #pragma ide diagnostic ignored "EndlessLoop"
   while (true) {
     //the WFE below wakes up at least every millisecond due to the systick being programmed for 1kHz.
+    //unlike Arduino we insist that something tangible happens for each loop. Everything interesting will cause an interrupt or event so this actually works just fine.
     MNE(WFE); //WFE is more inclusive than WFI, events don't call an isr but do wakeup the core.
-    loop();   
+    loop();  
   }
 #pragma ide diagnostic ignored "UnreachableCode"
   return 0;
