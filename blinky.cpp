94a4d1b56a36c70d6334ae68cf97982f6a664e4f
andyh <andy.980f@gmail.com>
2025-02-09 18:09:59 -0600

    about to add uart access

--- blinky.cpp
+++ blinky.cpp
@@ -5,25 +5,53 @@
 #include "msservice.h"
 RegisterPolledTimerWithSysTick;
 //above is like Arduino.h, will add more to it as we emulate the "always present" parts of Arduino, such as a default Serial.
-
+///////////////////////////////////////////////////////////////////////
 //Sketch Start
-#include "softpwm.h"
-MakeTimer(PolledSoftPWM, ledToggler, 750,250); 
+#include "sharedtimer.h"
+#include "gpio.h"
+
+class WaveFormer : public SharedTimer {
+protected:
+  OutputPin &pin;
+  unsigned *pattern;
+  unsigned phase;
+  public:
+    /** we override the 'count complete' event and switch to the other interval time value */
+  void onDone() override {
+    ++phase;
+    auto delay=pattern[phase];
+    if(delay==0){
+      delay=pattern[phase=0];
+    }
+    pin=phase&1;
+    restart(delay - 1);//# the sharedtimer stuff adds a 1 for good luck, we don't need no stinking luck. //todo: guard against a zero input
+  }
+  WaveFormer(OutputPin &ref,unsigned pattern[]):pin(ref),pattern(pattern){}
+  void setup(){
+    phase=0;
+    restart(*pattern-1);
+  }
+} ;
 
 
 #include "bluepill.h"  
 Bluepill board;
+unsigned patternMemory[]={750,250,100,800,0};
+unsigned COA=0;
+MakeTimer(WaveFormer, ledToggler, board.led, patternMemory); 
+
+
 
 void setup() {
   board.led=0;
-  ledToggler.trigger(1);
+  ledToggler.setup();
 }
 
 void loop() {
-  board.led=!ledToggler;
+  //all activity is in ISR's.
 }
 //Sketch End
-
+//////////////////////////////////////////////////////////////////////////
 //// below here is the equivalent of what Arduino build adds to your sketch:
 #include "core_cmInstr.h" //wfe/wfi
 int main(void) {
